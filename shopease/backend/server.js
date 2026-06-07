ef72e4fca45de96bb7b703628d8d2236391c3465
chandani yadav <chandani9189@gmail.com>
2026-06-07 17:02:37 +0530

    export app for vercel serverless

--- shopease/backend/server.js
+++ shopease/backend/server.js
@@ -14,7 +14,16 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', '*');
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  next();
+});
 
 app.use(express.json());
 
